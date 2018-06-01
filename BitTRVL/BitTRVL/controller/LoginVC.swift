640eb5722b8483f26f44a2cfb61fc10fa66f9acb
Jason Sanchez <sanchezjason@me.com>
2018-06-01 02:16:56 -0600

    RevealingSplashView

--- BitTRVL/BitTRVL/controller/LoginVC.swift
+++ BitTRVL/BitTRVL/controller/LoginVC.swift
@@ -12,10 +12,17 @@ class LoginVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.bindToKeyboard()
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleScreenTap(sender:)))
+        self.view.addGestureRecognizer(tap)
     }
     
-
+    @objc func handleScreenTap(sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
     
-
+    @IBAction func cancelButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
 }
