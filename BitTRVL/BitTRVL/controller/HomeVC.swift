f3189a3447927d52e3b34229bfef6aa106d5107b
san2ride <sanchezjason@me.com>
2018-08-21 02:00:20 -0600

    Removing Destination Coordinate from Firebase

--- BitTRVL/BitTRVL/controller/HomeVC.swift
+++ BitTRVL/BitTRVL/controller/HomeVC.swift
@@ -292,6 +292,18 @@ extension HomeVC: UITextFieldDelegate {
     func textFieldShouldClear(_ textField: UITextField) -> Bool {
         matchingItems = []
         tableView.reloadData()
+        
+        DataService.instance.REF_USERS.child(currentUserId!).child("tripCoordinate").removeValue()
+        
+        mapView.removeOverlays(mapView.overlays)
+        for annotation in mapView.annotations {
+            if let annotation = annotation as? MKPointAnnotation {
+                mapView.removeAnnotation(annotation)
+            } else if annotation.isKind(of: PassengerAnnotation.self) {
+                mapView.removeAnnotation(annotation)
+            }
+        }
+        
         centerMapOnUserLocation()
         return true
     }
