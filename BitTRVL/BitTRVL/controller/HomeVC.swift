61baa7a20e6d8a148b70450fdd1c4c54833671b9
san2ride <sanchezjason@me.com>
2018-09-04 02:52:31 -0600

    Configuring Start and End Trip: Getting Directions with AppleMaps

--- BitTRVL/BitTRVL/controller/HomeVC.swift
+++ BitTRVL/BitTRVL/controller/HomeVC.swift
@@ -387,9 +387,26 @@ class HomeVC: UIViewController, Alertable {
                 }
             })
         case .getDirectionsToDestination:
-            print("Got directions to destination")
+            DataService.instance.driverIsOnTrip(driverKey: self.currentUserId!, handler: { (isOnTrip, driverKey, tripKey) in
+                if isOnTrip == true {
+                    DataService.instance.REF_TRIPS.child(tripKey!).child("destinationCoordinate").observe(.value, with: { (snapshot) in
+                        let destinationCoordinateArray = snapshot.value as! NSArray
+                        let destinationCoordinate = CLLocationCoordinate2D(latitude: destinationCoordinateArray[0] as! CLLocationDegrees, longitude: destinationCoordinateArray[1] as! CLLocationDegrees)
+                        let destinationPlacemark = MKPlacemark(coordinate: destinationCoordinate)
+                        let destinationMapItem = MKMapItem(placemark: destinationPlacemark)
+                        
+                        destinationMapItem.name = "Passenger Destination"
+                        destinationMapItem.openInMaps(launchOptions: [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving])
+                    })
+                }
+            })
         case .endTrip:
-            print("Ended Trip")
+            DataService.instance.driverIsOnTrip(driverKey: self.currentUserId!, handler: { (isOnTrip, driverKey, tripKey) in
+                if isOnTrip == true {
+                    UpdateService.instance.cancelTrip(withPassengerKey: tripKey!, forDriverKey: driverKey!)
+                    self.buttonsForDriver(areHidden: true)
+                }
+            })
         }
     }
     
@@ -416,6 +433,7 @@ extension HomeVC: CLLocationManagerDelegate {
                 } else if region.identifier == "destination" {
                     self.cancelBtn.fadeTo(alphaValue: 0.0, withDuration: 0.2)
                     self.cancelBtn.isHidden = true
+                    self.actionForButton = .endTrip
                     self.actionButton.setTitle("END TRIP", for: .normal)
                 }
             }
@@ -556,9 +574,6 @@ extension HomeVC: MKMapViewDelegate {
             
             self.route = response.routes[0]
             self.mapView.add(self.route!.polyline)
-//            if self.mapView.overlays.count == 0 {
-//                
-//            }
             
             self.zoom(toFitAnnotationsFromMapView: self.mapView, forActiveTripWithDriver: false, withKey: nil)
             
